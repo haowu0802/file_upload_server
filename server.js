6bacf2ed2aeb80e5356f198f5c940e95b053bcd9
Hao Wu <haowu0802@gmail.com>
2017-08-12 18:38:11 -0700

    modulize server and put entry point into index

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
 let http = require("http");
 
-http.createServer(function (request, response) {
-    response.writeHead(200, {"Content-Type": "text/plain"});
-    response.write("Copyright 2017 TheSkillPanel.com All Rights Reserved.");
-    response.end();
-}).listen(8888);
+function start(){
+    function onRequest (request, response) {
+        console.log("Request received.");
+        response.writeHead(200, {"Content-Type": "text/plain"});
+        response.write("Copyright 2017 TheSkillPanel.com All Rights Reserved.");
+        response.end();
+    }
+
+    http.createServer(onRequest).listen(8888);
+    console.log("Server started.");
+}
+
+exports.start = start;
